e046b66e489f3609b48edb1e2c695790cd171377
hcw <976542897@qq.com>
2021-06-14 17:02:12 +0800

    创建分支1

--- myGit/src/main/java/com/hcw/git/HelloGit.java
+++ myGit/src/main/java/com/hcw/git/HelloGit.java
@@ -8,6 +8,6 @@ package com.hcw.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-
+        System.out.println("我是分支1");
     }
 }
