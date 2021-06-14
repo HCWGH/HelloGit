fd1ec5792ef13c5574b553424af171e42ae2a9b4
hcw <976542897@qq.com>
2021-06-14 16:48:44 +0800

    更新1

---
+++ myGit/src/main/java/com/hcw/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.hcw.git;
+
+/**
+ * @author HCW
+ * @className HelloGit
+ * @data 2021/6/14 16:46
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+
+    }
+}
