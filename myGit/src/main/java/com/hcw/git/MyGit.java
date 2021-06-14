2674f01c97a96b9dde0d7be6cb3d4d0857141da9
hcw <976542897@qq.com>
2021-06-14 16:01:02 +0800

    添加文件

---
+++ myGit/src/main/java/com/hcw/git/MyGit.java
@@ -0,0 +1,12 @@
+package com.hcw.git;
+
+/**
+ * @author HCW
+ * @className MyGit
+ * @data 2021/6/14 15:44
+ */
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("myGit");
+    }
+}
