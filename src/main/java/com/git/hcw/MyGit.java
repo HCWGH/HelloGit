17d7f11336ee208b4233844832af4153bb4ee728
hcw <976542897@qq.com>
2021-06-15 12:37:05 +0800

    hello

---
+++ src/main/java/com/git/hcw/MyGit.java
@@ -0,0 +1,12 @@
+package com.git.hcw;
+
+/**
+ * @author HCW
+ * @className MyGit
+ * @data 2021/6/15 12:34
+ */
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
