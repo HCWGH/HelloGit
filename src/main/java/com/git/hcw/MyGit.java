d8d160f3f9473dd4de8c17ee55a31b7c504fe683
hcw <976542897@qq.com>
2021-06-15 12:41:03 +0800

    dev分支添加

--- src/main/java/com/git/hcw/MyGit.java
+++ src/main/java/com/git/hcw/MyGit.java
@@ -8,5 +8,7 @@ package com.git.hcw;
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("主分支添加");
+        System.out.println("dev分支添加2");
     }
 }
